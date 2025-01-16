032422cfb4b57845f57d892b890934d3074d1bba
LorenzoBellidoB <lorenzo.bellido.b@gmail.com>
2025-01-16 10:15:00 +0100

    color

--- Portfolio/Portfolio/views/footer/Footer.py
+++ Portfolio/Portfolio/views/footer/Footer.py
@@ -5,14 +5,48 @@ from Portfolio.views.texts.texts import parrafo
 
 
 def footer() -> rx.Component:
-    return rx.hstack(
-        rx.icon(
+    return rx.vstack(
+        rx.hstack(
+            rx.link(
+                rx.icon(
+                    "github",
+                    color="white",
+                    size=35
+                ),
+                href="https://github.com/LorenzoBellidoB"
+                
+            ),
+            rx.link(
+            rx.icon(
+                "twitter",
+                color="white",
+                size=35
+            ),
+            href="https://x.com/lorenzobeba2"
+        ),
+        rx.link(
+            rx.icon(
+                "instagram",
+                color="white",
+                size=35
+            ),
+            href="https://instagram.com/lorenzobellidoo"
+        ),
+        spacing="5"
+        ),
+        
+        rx.hstack(
+            rx.icon(
         "copyright",
         width="20px",
     ),
     rx.link(
         f"{datetime.date.today().year} Pagina de Lorenzo Bellido",
         href="http/localhost:3000/",
+        color="white",
         underline="none",
+        style={":hover":{"color":"white"}}
     )
+        ),
+        align="center"
     ) 
--- Portfolio/Portfolio/views/navBar/BarraNav.py
+++ Portfolio/Portfolio/views/navBar/BarraNav.py
@@ -5,10 +5,9 @@ from Portfolio.views.components.ButtonLinks import buttonLink
 def barranavegacion() -> rx.Component:
     return rx.hstack(
         buttonLink("Home", "http/localhost:3000/", "#FFB200"),
-        buttonLink("Git", "https://github.com/LorenzoBellidoB?tab=repositories", "#601d98"),
         buttonLink("Projects", "http/localhost:3000/", "#E05600"),
+        buttonLink("Linkedin", "www.linkedin.com/in/lorenzo-bellido-barrena", "#008176"),
         buttonLink("Contact", "http/localhost:3000/", "#007400"),
-        buttonLink("Curriculum", "http/localhost:3000/", "#008176"),
         position="sticky",
         padding="10px",
         zindex="1",
